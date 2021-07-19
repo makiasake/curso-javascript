de785abb77c6f02475deba42a23008d3dacb625c
makiasake <alexandre.maki@hotmail.com>
2021-07-19 14:39:14 -0300

    usandolet1, usando var em loop 1-2, usando let em loop 1-2, Hoisting, function vs object, par nome/valor, notação ponto, operadores atribuição

---
+++ Fundamentos/parChaveValor.js
@@ -0,0 +1,20 @@
+const saudacao = 'Oi'
+
+function exec() {
+    const saudacao = 'Falaaa'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Pedro',
+    idade: 32,
+    peso: 90,
+    endereco: {
+        logradouro: '',
+        numero: 1
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
