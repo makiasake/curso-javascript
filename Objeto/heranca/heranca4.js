4668c1d1717702d35e45e18e1d3a2e907ebbd61f
makiasake <alexandre.maki@hotmail.com>
2022-06-13 11:16:40 -0300

    Heranca 4

---
+++ Objeto/heranca/heranca4.js
@@ -0,0 +1,29 @@
+function MeuObjeto() {
+    console.log(MeuObjeto.prototype)
+}
+
+const obj1 = new MeuObjeto
+const obj2 = new MeuObjeto
+
+// os dois objetos tem o mesmo prototipo?
+console.log(obj1.__proto__ === obj2.__proto__)
+console.log(MeuObjeto.prototype === obj2.__proto__)
+
+MeuObjeto.prototype.nome = 'Anonimo'
+MeuObjeto.prototype.falar = function () {
+    console.log(`Bom dia! Meu nome é ${this.nome}!`)
+}
+
+obj1.falar()
+
+obj2.nome = 'Rafael'
+obj2.falar()
+
+const obj3 = {}
+obj3.__proto__ = MeuObjeto.prototype
+obj3.nome = 'Obj3'
+obj3.falar()
+
+console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
+console.log(MeuObjeto.__proto__)
+console.log(Function.prototype.__proto__ === Object.prototype)
