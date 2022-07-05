b343998957de8da8d43e2e861ae6737bf2aea621
makiasake <alexandre.maki@hotmail.com>
2022-07-05 20:29:29 -0300

    Instalando Deps e scripts, leitura de arquivos

---
+++ Node/Arquivos/leitura.js
@@ -0,0 +1,26 @@
+// filesystem
+const fs = require('fs')
+
+const caminho = __dirname + '/arquivo.json'
+
+// sincrono...
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+// assincrono
+fs.readFile(caminho, 'utf-8', (error, conteudo) => {
+    if (error) {
+        console.log(erro)
+    } else {
+        const config = JSON.parse(conteudo)
+        console.log(`${config.db.host}:${config.db.port}`)
+    }
+})
+
+const config = require('./arquivo.json')
+console.log(config.db)
+
+fs.readdir(__dirname, (error, arquivos) => {
+    console.log('Conteúdo da pasta...')
+    console.log(arquivos)
+})
