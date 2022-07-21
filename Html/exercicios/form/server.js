7420f4405783984a35ace97a1bf47d4579ac2b40
makiasake <alexandre.maki@hotmail.com>
2022-07-21 20:48:10 -0300

    HTML: Formulário #02 e #03

--- Html/exercicios/form/server.js
+++ Html/exercicios/form/server.js
@@ -8,4 +8,16 @@ app.post('/usuarios', (req, res) => {
     res.send('Cadastrado')
 })
 
+app.post('/usuarios/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    console.log(req.body)
+    res.send('Salvo usuário' + id)
+})
+
+app.get('/usuarios', (req, res) => {
+    console.log(req.query)
+    res.send('Cadastrado')
+})
+
 app.listen(3003)
